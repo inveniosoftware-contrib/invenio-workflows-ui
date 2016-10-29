03250038a830cb088a4effff51922a34d5414b50
David Caro <david@dcaro.es>
2016-10-29 11:04:12 +0200

    packaging: remove old python compatibility tags
    Signed-off-by: David Caro <david@dcaro.es>

--- setup.py
+++ setup.py
@@ -132,8 +132,6 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Development Status :: 1 - Planning',
         ],
