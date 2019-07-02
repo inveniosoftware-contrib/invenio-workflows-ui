d0d32a8dfae58e9b350ec39f5bf60b8dbc3ea346
Tamas Nyiri <tamas.nyiri@cern.ch>
2019-07-02 15:13:11 +0200

    upgrade to ES7

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ SETUP_REQUIRES = [
 
 INSTALL_REQUIRES = [
     'autosemver~=0.2,>=0.2',
-    'elasticsearch-dsl~=5.0',
+    'elasticsearch-dsl~=7.0',
     'Flask-BabelEx>=0.9.2',
     'invenio-access~=1.0,>=1.0.1',
     'invenio-search>=1.0.0a5',
